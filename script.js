0021d46fbe05af9054015485d78c30f21a5a905b
AlfredEVOL <57448041+AlfredEVOL@users.noreply.github.com>
2020-11-15 01:44:06 +0530

    Revert "."
    This reverts commit 2d5d54eb0b90057fbba3b042500cb9b8efd6f8a3.

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-function imageSubmit(event) {
-  if (event.keyCode == 13) {
-    console.log("New Image");
-    var imgURL = document.getElementById("imageURL").value;
-    var list = document.getElementById("imgView");
-    list.removeChild(list.childNodes[0]);
-    console.log(imgURL);
-    var img = document.createElement("img")
-    img.src = imgURL;
-    list.appendChild(img);
-  }
-}
+function imageSubmit(event ){
+    if(event.keyCode==13){
+        console.log("New Image")
+    }
+}   
