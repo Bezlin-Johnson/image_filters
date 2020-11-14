2d5d54eb0b90057fbba3b042500cb9b8efd6f8a3
AlfredEVOL <57448041+AlfredEVOL@users.noreply.github.com>
2020-11-14 20:44:23 +0530

    .

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function imageSubmit(event ){
     if(event.keyCode==13){
         console.log("New Image")
+        imgURL=document.getElementById("imageURL").value;
+        console.log(imgURL);
+        document.getElementById("imageURL").src=imgURL;
+        window.location.reload();
+
     }
 }   
