c4f2b22c1fa68790b5a41142d88a6c3a8e0078a3
AlfredEVOL <57448041+AlfredEVOL@users.noreply.github.com>
2020-11-14 21:31:39 +0530

    .

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function imageSubmit(event ){
-    if(event.keyCode==13){
-        console.log("New Image")
-        imgURL=document.getElementById("imageURL").value;
-        console.log(imgURL);
-        document.getElementById("imageURL").src=imgURL;
-        window.location.reload();
-
-    }
-}   
+function imageSubmit(event) {
+  if (event.keyCode == 13) {
+    console.log("New Image");
+    var imgURL = document.getElementById("imageURL").value;
+    var list = document.getElementById("imgView");
+    list.removeChild(list.childNodes[0]);
+    console.log(imgURL);
+    var img = document.createElement("img")
+    img.src = imgURL;
+    list.appendChild(img);
+  }
+}
